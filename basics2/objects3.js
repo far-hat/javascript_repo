b88f99e357f58710faaadd48aae4cd31e339f7d8
far-hat <160326106+far-hat@users.noreply.github.com>
2024-04-30 14:24:43 +0000

    destructuring , json talk

---
+++ basics2/objects3.js
@@ -0,0 +1,20 @@
+const course ={
+    courseName : "Javascript",
+    price : 499,
+    courseInstructor : "Hitesh"
+}
+
+const {courseInstructor : cNAme} = course //destructuring of object elemnts
+console.log(cNAme)
+
+//--------------------------------
+//###########  API    ###########
+/* Json is an object without a name used in React
+*/
+
+// {
+//     "name":"farhat",
+//     "courseName" : "Javascript",
+//     "price" : "free"
+// }
+
