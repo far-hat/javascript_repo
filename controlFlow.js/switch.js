89b6271062c25285f785f567e1fbe4f0871e4626
far-hat <160326106+far-hat@users.noreply.github.com>
2024-05-03 14:02:31 +0000

    switch statement

---
+++ controlFlow.js/switch.js
@@ -0,0 +1,15 @@
+let color = 'red';
+switch (color){
+    case 'red':
+        console.log("Stop");
+        break;
+    case 'yellow':
+        console.log("Wait");
+        break;
+    case 'green':
+        console.log("Go");
+        break;
+    default:
+        console.log("Wrong input");
+        break;
+}
